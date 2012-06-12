f971fc5133407e6de4c8f79ab460aa568f1e0d80
Topper Bowers <topper@toppingdesign.com>
2012-06-12 10:59:52 -0700

    very default webapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TriniTester::Application.config.secret_token = '620cc6ee28a9dfad2e4ca36b3bffbd1f33c44dcc7501bf3403d0631eb962979c7a6fbccc9fa87ddbf3af90c890cb91b6d2a229250ab447ab39ed61d3523468ce'
